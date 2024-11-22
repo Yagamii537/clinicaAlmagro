591d561c6de625c57b672f72a853d04124cac932
Yagami537 <mauriciosistemas18@gmail.com>
2024-11-22 15:14:33 -0500

    inicio del sistema

---
+++ src/clinicaalmagro/ClinicaAlmagro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clinicaalmagro;
+
+/**
+ *
+ * @author Yagami
+ */
+public class ClinicaAlmagro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
